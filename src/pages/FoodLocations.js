3c74d7cf1a9842af124e4668fe3260f58038ea0f
ryanacharya <ryan2715@student.ubc.ca>
2021-12-27 15:49:26 -0700

    Testing current outputs

--- src/pages/FoodLocations.js
+++ src/pages/FoodLocations.js
@@ -1,9 +1,13 @@
+import ImageComponent from "../components/FoodLoactions/ImageComponent"
+import LocationsComponent from "../components/FoodLoactions/LocationsComponent"
 import Navbar from "../components/Navbar/Navbar"
 
 const FoodLocations = () => {
     return (
         <div>
             <Navbar />
+            <LocationsComponent />
+            <ImageComponent />
         </div>
     )
 }
