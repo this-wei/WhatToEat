14de95e7f78d37d52d7cba680660f4e361205513
weijinluo <weijinluo@gmail.com>
2021-12-28 21:45:10 -0700

    Got rid of title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,6 @@ import './Home.css'
 const Home = () => {
     return (
         <>
-            <div className='header'>
-                <h1 className='title'>What to Eat</h1>
-                <p className='subtitle'>Don't know what to eat? Let us help!</p> 
-            </div>
             <Form />
         </>
     )
